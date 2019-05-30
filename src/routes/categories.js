b0fd23ef52f7dc39a41d4a42e1b9333d135ad5ea
RoryLetteney <rory.letteney@gmail.com>
2019-05-30 15:40:03 -0500

    Moved the endpoints for each route to the proper route file.

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -5,6 +5,10 @@ const {
 } = require('express');
 const router = new Router();
 
-
+router.get('/categories', getCategories);
+router.post('/categories', postCategories);
+router.get('/categories/:id', getCategory);
+router.put('/categories/:id', putCategories);
+router.delete('/categories/:id', deleteCategories);
 
 module.exports = router;
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,6 +5,10 @@ const {
 } = require('express');
 const router = new Router();
 
-
+router.get('/products', getProducts);
+router.post('/products', postProducts);
+router.get('/products/:id', getProduct);
+router.put('/products/:id', putProducts);
+router.delete('/products/:id', deleteProducts);
 
 module.exports = router;
